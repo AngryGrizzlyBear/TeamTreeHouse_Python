60541389eff939fb90f991079873b196b5163861
Evan <evanmlongwood@gmail.com>
2018-03-22 06:18:36 -0700

    Splitting and Joining

---
+++ Python Basics/Stage 1/splitting_and_joining.py
@@ -0,0 +1,9 @@
+list('hello')
+'hello'.split()
+'hello there students'.split()
+"red:blue:green".split()
+
+flavors = ['chocolate', 'mint', 'strawberry']
+','.join(flavors)
+print("My favorite flavors are: " + ', '.join(flavors))
+print("My favorite flavors are: {}".format(", ".join(flavors)))
